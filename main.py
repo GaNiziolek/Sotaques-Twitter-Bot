7524dd17cef120e983889912749150907e7693f9
GaNiziolek <54728889+GaNiziolek@users.noreply.github.com>
2020-08-17 23:51:09 -0300

    a

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tweepy
 import datetime
 from time import sleep
-import logging
 from os import environ
 import psycopg2
+import traceback
 
 DATABASE_URL = environ['DATABASE_URL']
 
@@ -26,9 +26,10 @@ def create_api():
     try:
         api.verify_credentials()
     except Exception as e:
-        logging.error('Error creating API', exc_info=True)
+        print('Error creating API')
+        print(traceback.format_exc())
         raise e
-    logging.info('API created')
+    print('API created')
 
     return api
 
@@ -48,14 +49,14 @@ def tweetar(api, msg, reply_to=None):
             raise error
 
 def check_mentions(api, since_id):
-    logging.info('Retrieving mentions')
+    print('Retrieving mentions')
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
         
-        logging.info(f'Answering to {tweet.user.name}')
+        print(f'Answering to {tweet.user.name}')
 
         if not tweet.user.following:
             tweet.user.follow()
@@ -63,15 +64,17 @@ def check_mentions(api, since_id):
         #tweetar(api, 
         #        f'Olá {tweet.user.name} ainda estou em testes, não sei responder muita coisa.',
         #        reply_to=tweet.id)
+
+
         text = tweet.text
         if 'significa' in tweet.text:
 
             if '@tradubot' in text:
-                text.replace('@tradubot','')
+                text = text.replace('@tradubot','')
 
             words = tweet.text.split('significa')
             print(words[0] + ' para ' + words[1])
-            sql = 'insert into Tradutor(Base_word, Trans_word) values ({}, {})'.format(words[0], words[1])
+            sql = 'insert into Tradutor(Base_word, Trans_word) values ("{}", "{}")'.format(words[0], words[1])
 
 
     return new_since_id
@@ -89,7 +92,7 @@ def main():
 
         environ['SINCE_ID'] = str(since_id)
 
-        logging.info('Waiting...')
+        print('Waiting...')
         sleep(60)
 
 if __name__ == '__main__':
